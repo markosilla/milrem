5244792e0819d6098362aabfda1c05fd3223fac0
Marko Silla <marko.silla@gmail.com>
2024-07-30 00:57:07 +0300

    Add VSCode debug setup for udp_node

--- build.sh
+++ build.sh
@@ -5,23 +5,27 @@ clean () {
 }
 
 usage () {
-  echo "Usage: $0 [-c]"
+  echo "Usage: $0 [-c] [-a <args>]"
   echo -e "Builds Milrem AS test work\n"
   echo "Optional arguments:"
-  echo -e "  -c\tClean build"
+  echo -e "  -c       \tClean build"
+  echo -e "  -a <args>\tSet CMAKE args (e.g.-DCMAKE_BUILD_TYPE=Debug)"
   exit 0
 }
 
 build () {
   mv CMakeLists.txt CMakeLists.txt.old
-  colcon build --merge-install
+  colcon build --merge-install ${CMAKE_ARGS}
   mv CMakeLists.txt.old CMakeLists.txt
 }
 
-while getopts "c" flag
+CMAKE_ARGS=""
+
+while getopts "ca:" flag
 do
     case "${flag}" in
         c) clean;;
+        a) CMAKE_ARGS="--cmake-args ${CMAKE_ARGS} $OPTARG";;
         *) usage;;
     esac
 done
